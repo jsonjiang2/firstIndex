878841d50579b43b18e918763e7fa3ace100b4de
jsonjiang <jsonjiang@aliyun.com>
2019-05-23 22:34:16 +0800

    首页

---
+++ src/main/java/com/walmart/dao/UserMapper.java
@@ -0,0 +1,5 @@
+package com.walmart.dao;
+
+public class UserMapper {
+
+}
---
+++ src/main/java/com/walmart/service/impl/UserServiceImpl.java
@@ -0,0 +1,10 @@
+package com.walmart.service.impl;
+
+import org.springframework.stereotype.Service;
+
+import com.walmart.service.UserService;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+}
